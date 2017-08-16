d3ee377d2782101db5614e3252fecb2eeba8ce3b
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-08-16 23:57:18 +0700

    initial new project tim kiem noi dung dung AngularJS

--- angular/handle_event.js
+++ angular/handle_event.js
@@ -1,10 +1,18 @@
 var myApp = angular.module('eventDemo',[]);
 myApp.controller('eventController',function () {
     var handleEvent = this;
-    handleEvent.technologys =[
-        {name:'C#', like:'0', dislike:'0'},
-        {name:'JavaScript', like:'0', dislike:'0'},
-        {name:'AngularJS', like:'0', dislike:'0'},
-        {name:'PHP', like:'0', dislike:'0'}
-    ]
+    handleEvent.technologies =[
+        {name:'C#', like: 0, dislike: 0},
+        {name:'JavaScript', like: 0, dislike: 0},
+        {name:'AngularJS', like: 0, dislike: 0},
+        {name:'PHP', like: 0, dislike: 0}
+    ];
+    handleEvent.increaseLike= function (technology) {
+        technology.like++;
+
+    };
+    handleEvent.increaseDislike = function (technology) {
+        technology.dislike++;
+
+    }
 });
