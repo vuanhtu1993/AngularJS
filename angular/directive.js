40fa991d4b3252f8d6ab74ab71df70fc8362f998
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-08-16 14:59:04 +0700

    create new directive

--- angular/directive.js
+++ angular/directive.js
@@ -2,4 +2,8 @@ var myApp = angular.module('DirectiveDemo',[]);
 
 myApp.controller('directiveController',function () {
     
-})
+}).directive('welcomeText',function () {
+    return {
+        template:'Welcome to my first Angular'
+    }
+});
