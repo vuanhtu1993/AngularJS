3a3424c51e7cad89ab78d76d76df569c2ee64aeb
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-08-16 15:13:13 +0700

    tác dụng rất lớn của custom Directive là có thể định nghĩa mỗi view là một directive

--- angular/directive.js
+++ angular/directive.js
@@ -1,9 +1,10 @@
 var myApp = angular.module('DirectiveDemo',[]);
 
-myApp.controller('directiveController',function () {
-    
+myApp.controller('directiveController',function ($scope) {
+
 }).directive('welcomeText',function () {
     return {
-        template:'Welcome to my first Angular'
+        //template:'Welcome to my first Angular'
+        templateUrl:'customDirective.html'
     }
 });
