87e5dcf2d8cf68b09845a51f5f52e2e18c7a9dd3
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-08-16 11:01:51 +0700

    pham vi cua bien $rootScope là có thể sử dụng trong, các controller khác nhau trong ng-app

--- angular/scope.js
+++ angular/scope.js
@@ -1,9 +1,10 @@
 var myApp = angular.module('scopeDemo',[]);
 
-myApp.controller('classController',function ($scope) {
-    $scope.name = 'JavaSciprt';
+myApp.controller('classController',function ($scope,$rootScope) {
+
+    $rootScope.name = 'Javescript';
 });
 myApp.controller('schoolController',function ($scope) {
-    $scope.name = 'Angular';
+
 });
 
