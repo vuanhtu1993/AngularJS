056d368eb518d551a166aaaa658f7a39f6ef08fa
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-08-18 02:06:23 +0700

    used filter of Angular

--- angular/filter.js
+++ angular/filter.js
@@ -1,4 +1,11 @@
-var myApp = angualar.module('filterDemo',[]);
+var myApp = angular.module('filterDemo',[]);
 myApp.controller('filterController',function () {
-    
-})
+    var filter = this;
+    filter.employees = [
+        {name:'John', birthday:new Date('6/6/1993'), salary:'1000',gender:'male'},
+        {name:'Jane', birthday:new Date('6/7/1993'), salary:'2000',gender:'female'},
+        {name:'Ruby', birthday:new Date('6/8/1993'), salary:'3000',gender:'female'},
+        {name:'Shapia', birthday:new Date('6/9/1993'), salary:'6000',gender:'female'},
+        {name:'Mark', birthday:new Date('6/10/1993'), salary:'8000',gender:'male'}
+    ]
+});
