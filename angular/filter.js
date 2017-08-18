1ca60d321dc8989535a3843d57624c9d6c96d396
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-08-18 11:18:09 +0700

    used filter number of row

--- angular/filter.js
+++ angular/filter.js
@@ -2,10 +2,18 @@ var myApp = angular.module('filterDemo',[]);
 myApp.controller('filterController',function () {
     var filter = this;
     filter.employees = [
-        {name:'John', birthday:new Date('6/6/1993'), salary:'1000',gender:'male'},
-        {name:'Jane', birthday:new Date('6/7/1993'), salary:'2000',gender:'female'},
-        {name:'Ruby', birthday:new Date('6/8/1993'), salary:'3000',gender:'female'},
-        {name:'Shapia', birthday:new Date('6/9/1993'), salary:'6000',gender:'female'},
-        {name:'Mark', birthday:new Date('6/10/1993'), salary:'8000',gender:'male'}
+        {name:'John', birthday:new Date('6/6/1993'), salary:'1000',gender:'male',status:true},
+        {name:'Jane', birthday:new Date('6/7/1993'), salary:'2000',gender:'female',status:false},
+        {name:'Ruby', birthday:new Date('6/8/1993'), salary:'3000',gender:'female',status:true},
+        {name:'Shapia', birthday:new Date('6/9/1993'), salary:'6000',gender:'female',status:true},
+        {name:'Mark', birthday:new Date('6/10/1993'), salary:'8000',gender:'male',status:false},
+        {name:'May', birthday:new Date('5/10/1993'), salary:'8000',gender:'female',status:true}
     ]
+});
+myApp.filter('status',function () {
+    return function (input) {
+        if(input == true)
+            return 'available';
+        else return'unavailable';
+    }
 });
